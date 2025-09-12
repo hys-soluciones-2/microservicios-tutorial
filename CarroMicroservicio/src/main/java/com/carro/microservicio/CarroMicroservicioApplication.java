1af7b51c62ce0e2d119180c112179d163abd63fc
hys-soluciones-2 <hugoeduardo0716@gmail.com>
2025-09-12 11:56:50 -0500

    Eureka

--- CarroMicroservicio/src/main/java/com/carro/microservicio/CarroMicroservicioApplication.java
+++ CarroMicroservicio/src/main/java/com/carro/microservicio/CarroMicroservicioApplication.java
@@ -2,8 +2,10 @@ package com.carro.microservicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarroMicroservicioApplication {
 
 	public static void main(String[] args) {
